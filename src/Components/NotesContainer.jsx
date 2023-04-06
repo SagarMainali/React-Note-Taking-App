9e20fcdee69d8b776fefc32d5b9da50cd6591ad0
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-06 17:35:40 +0545

    fixed spacing

--- src/Components/NotesContainer.jsx
+++ src/Components/NotesContainer.jsx
@@ -3,8 +3,11 @@ import Note from './Note'
 
 function NotesContainer(props) {
      return (
-          <div className='notes-container flex'>
-               {props.notesData.map((item, index) => <Note key={index} {...item} />)}
+          <div className='notes-main-container'>
+               <h1>Notes App</h1>
+               <div className='notes-container flex'>
+                    {props.notesData.map((item, index) => <Note key={index} {...item} />)}
+               </div>
           </div>
      )
 }
